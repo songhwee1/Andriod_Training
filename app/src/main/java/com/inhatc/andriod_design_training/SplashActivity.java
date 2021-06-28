d760986c3ef5d871f91efc2f150711004610ae03
songhwee1 <songhwee1@naver.com>
2021-06-28 14:22:35 +0900

    resize img

--- app/src/main/java/com/inhatc/andriod_design_training/SplashActivity.java
+++ app/src/main/java/com/inhatc/andriod_design_training/SplashActivity.java
@@ -15,16 +15,12 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
         decorView = getWindow().getDecorView();
         uiOption = getWindow().getDecorView().getSystemUiVisibility();
-        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
-            uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
-            uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
-            uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         decorView.setSystemUiVisibility( uiOption );
 
