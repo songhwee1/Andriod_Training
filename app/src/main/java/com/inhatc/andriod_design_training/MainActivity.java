11fa85c817a90a4ca2a0748f4b198b56f6508fd2
songhwee1 <songhwee1@naver.com>
2021-06-28 17:46:05 +0900

    add register page

--- app/src/main/java/com/inhatc/andriod_design_training/MainActivity.java
+++ app/src/main/java/com/inhatc/andriod_design_training/MainActivity.java
@@ -2,12 +2,22 @@ package com.inhatc.andriod_design_training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private View decorView;
+    private int	uiOption;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        decorView.setSystemUiVisibility( uiOption );
+
     }
 }
