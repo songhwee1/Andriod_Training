adc476a8194768d4a25721835946209b6deeb6b2
songhwee1 <songhwee1@naver.com>
2021-06-28 11:43:39 +0900

    add img, set splash view

--- app/src/main/java/com/inhatc/andriod_design_training/SplashActivity.java
+++ app/src/main/java/com/inhatc/andriod_design_training/SplashActivity.java
@@ -1,6 +1,47 @@
 package com.inhatc.andriod_design_training;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
-public class SplashActivity  extends AppCompatActivity {
+public class SplashActivity extends Activity {
+    private View decorView;
+    private int	uiOption;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
+            uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+            uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
+            uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        decorView.setSystemUiVisibility( uiOption );
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 3000);
+
+    }
+
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
 }
